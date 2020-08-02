af92e1f99429cd4827605f346e9ab40d24fe32a9
GNH7HC <huuthanhhung@gmail.com>
2020-08-02 10:38:06 +0700

    demo_server_1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set("views", "./views");
 
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);// set up SOCKET.IO
-server.listen(process.env.PORT || 2020);
+server.listen(process.env.PORT || 2802);
 
 var ArrUser = [];
 // listen to creat connection if recieve request
